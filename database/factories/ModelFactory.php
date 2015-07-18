d27d5421aff677cfe0bc1c5750ffaa1d1391a3ea
alfredudu <alfredudu_007@hotmail.com>
2015-07-18 18:47:44 -0300

    Adicionando dados

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,22 @@
 |
 */
 
-$factory->define(LaravelProject\User::class, function ($faker) {
+$factory->define(LaravelProject\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
         'password' => str_random(10),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(LaravelProject\Client::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'responsible' => $faker->name,
+        'email' => $faker->email,
+        'phone' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'obs' => $faker->sentence,
     ];
 });
